75c8160958f1d8e9ee267884595916e146ba6780
cj-cloud2 <cj.cloud.002@gmail.com>
2025-08-11 09:04:10 +0530

    Delete Mock Assignments Version 1 directory

--- Mock Assignments Version 1/v3/LoanStatus.java
+++
@@ -1,9 +0,0 @@
-package org.p011_mocks2.v3;
-
-// Enum to represent loan statuses
-public enum LoanStatus {
-    PENDING,
-    APPROVED,
-    REJECTED,
-    VERIFICATION_PENDING
-}
--- Mock Assignments Version 1/v4/DocumentValidationService.java
+++
@@ -1,6 +0,0 @@
-package org.p011_mocks2.v4;
-
-// New interface for document validation service integration
-public interface DocumentValidationService {
-    DocumentValidationStatus validateDocuments(String applicantId) throws RuntimeException;
-}
